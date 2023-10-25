65a47d25370e0be37903552f2cc390f5ef4449d7
txbxxx <2865260231@qq.com>
2023-10-25 14:06:08 +0800

    WebApp

--- ll_env/learning_logs/models.py
+++ ll_env/learning_logs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+"""定义主题模型"""
 class Topic(models.Model):
     """属性Text是由字符组成的数据，就是文本，需要存储少量的文本文件，名称、标题等，且需要高数Django在数据库中预留多少空间即max_length"""
     text = models.CharField(max_length=200)
@@ -11,4 +11,23 @@ class Topic(models.Model):
     """当使用print输出对象的时候，只要自己定义了__str__(self)方法，那么就会打印从在这个方法中return的数据"""
     def __str__(self):
         """默认使用text格式来来显示有关主题的信息，返回存储在属性text中的字符串"""
-        return self.text
+        return self.text,self.date_added
+
+
+"""定义主题类"""
+class Entry(models.Model):
+    """
+    创建外键，每创建一个主题时就会分配一个建。需要在两项数据建立连接的时候,Django就会使用键
+    on_delete=models.CASCADE 表示删除主题的同时就会删除与之相关的所有条目，这被称为级联删除
+    """
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    text = models.TextField()
+    data_added = models.DateTimeField(auto_now_add=True)
+    
+    """用于管理模型额外的消息，"""
+    class Mate:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        return f"{self.text[:50]}..."
+    
